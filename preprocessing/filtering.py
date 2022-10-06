b0e617a855e4e9618edf0ec106083aff3ac2e521
Tim-Tadj <77534029+Tim-Tadj@users.noreply.github.com>
2022-10-07 09:54:55 +1000

    added quantile derivative method

---
+++ preprocessing/filtering.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def quantile_derivative_autothresh(array:np.ndarray, ord:int, n_points=100):
+
+    if len(array.shape) != 1:
+        print("ERR: array must be 1d")
+        return
+    if ord < 0:
+        print("ERR: ord gradient must  be >= 0")
+        return
+    if n_points < 2:
+        print("ERR must have at least 2 quantiles")
+
+    spikes =[]
+    quantiles = np.arange(0, 1, 1/n_points)
+    quant = np.quantile(data, quantiles)
+    quant_grad = quant
+
+    for i in range(0, ord):
+        quant_grad = np.gradient(quant_grad)
+    
+    thresh = quant_grad.std()
+    
+    for i in range(len(quant_grad)):
+        if abs(quant_grad[i]) > thresh:
+            spikes.append(i)
+
+    return np.array(spikes)
+
+def min_max_thresh_QDautothresh(array:np.ndarray, ord:int, n_points=100):
+    spikes = quantile_derivative_autothresh(df_filtered['temperature'], ord, n_points=n_points)
+    min_thresh = spikes[spikes < n_points/2].max()
+    max_thresh = spikes[spikes > n_points/2].min()
+
+    filtered = array[ (array > np.quantile(array, min_thresh/n_points)) & (array < np.quantile(array, max_thresh/n_points))]
+    return filtered
+    
+
