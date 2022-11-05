d74f6d58e779209e23196d42c8de10f374665c4b
Tim-Tadj <77534029+Tim-Tadj@users.noreply.github.com>
2022-11-05 10:14:52 +1000

    added polyfit

--- preprocessing/regression.py
+++ preprocessing/regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def even_fit_y(y, deg=1):
+def even_fit_y(y: np.ndarray, deg=1):
     x = np.arange(len(df), dtype=float)
     consts = np.polyfit(x=x, y=y, deg=deg)
     ynew = np.zeros_like(x, dtype=float)
